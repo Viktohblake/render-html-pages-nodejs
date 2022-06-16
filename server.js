2fc30b8d8307758f3e694267c6713e077761023e
viktohblake <damanygt@gmail.com>
2022-06-16 12:05:47 +0100

    new changes

--- server.js
+++ server.js
@@ -1,41 +1,40 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
 const port = 4000;
 
 // create a server with the HTTP variable
 const server = http.createServer(function (request, response) {
-
   // create routes and send back some information
-  let path = '';
-  switch(request.url) {
-    case '/home':
-        path = 'home.html';
-        break;
-    case '/about':
-        path = 'about.html';
-        break;
-    case '/contact':
-        path = 'contact.html';
-        break;
+  let path = "";
+  switch (request.url) {
+    case "/home":
+      path = "home.html";
+      break;
+    case "/about":
+      path = "about.html";
+      break;
+    case "/contact":
+      path = "contact.html";
+      break;
     default:
-        break;    
+      break;
   }
 
   // render html file
-  if(path) {
+  if (path) {
     fs.stat(`./${path}`, (err, stats) => {
-		response.statusCode = 200;
-        // header
-		response.setHeader('Content-Type', 'text/html');
-  		if(stats) {
-		  	fs.createReadStream(path).pipe(response);
-  		} else {
-  			response.statusCode = 404;
-  			response.end('Sorry, page not found!');
-            console.log(err);
-  		}
-  	});
+      response.statusCode = 200;
+      // header
+      response.setHeader("Content-Type", "text/html");
+      if (stats) {
+        fs.createReadStream(path).pipe(response);
+      } else {
+        response.statusCode = 404;
+        response.end("Sorry, page not found!");
+        console.log(err);
+      }
+    });
   }
 });
 
